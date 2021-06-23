c59cc6424958bee92d74e3e9f8d52397100e176c
mudssky <mudssky@gmail.com>
2021-06-23 16:21:38 +0800

    ✨ feat: add concatScript
    add concatScript.ps1 and userscriptHead

---
+++ userscript-ts+vue3/src/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tslib_1 = require("tslib");
+// index.ts
+var vue_1 = require("vue");
+var App_vue_1 = tslib_1.__importDefault(require("./App.vue"));
+vue_1.createApp(App_vue_1.default).mount('#app');
+// const id = `myuser`
+// const div = document.createElement('div')
+// div.setAttribute('id', id)
+// document.body.appendChild(div)
+// createApp(App).mount(`#${id}`)
